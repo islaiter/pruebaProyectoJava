3a800521af733b003ac1326d232c684dbaca9244
islaiter <45145344+islaiter@users.noreply.github.com>
2021-01-17 21:36:23 +0100

    Update NumeroEntero.java

--- src/main/NumeroEntero.java
+++ src/main/NumeroEntero.java
@@ -4,17 +4,6 @@ public class NumeroEntero {
 	/* Dejamos todo preparado por si queremos añadir soporte para numeros decimales
 	 pero como es una clase simple de prueba, implementamos numeros enteros simples */
 	
-	// Prueba smee.io
-	// Prueba smee.io 200 OK 17/01/2021 21.02 GMT+1
-	// Otra prueba
-	
-	// Prueba ngrok2
-	// Prueba ngrok3
-	// Prueba ngrok4
-	// Prueba ngrok5
-	// Prueba ngrok6
-	
-	
 	
 	public Integer parteEntera;
 	
